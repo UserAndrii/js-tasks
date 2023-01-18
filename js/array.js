413a769734d244b4a16de7fcb71ef3be668534f1
Andrii Gadar <gadar.andre@gmail.com>
2023-01-18 14:06:05 +0200

    task1-7

---
+++ js/array.js
@@ -0,0 +1,30 @@
+// 1. Оголоси три змінні і присвой кожній з них значення,
+// використовуючи нотацію квадратних дужок.
+
+// 2. Виконай перевизначення значення елементів з індексами 1 і 3.
+// Заміни "plum" на "peach", а "orange" на "banana".
+
+// 3. Оголоси змінну fruitsArrayLength і присвой їй довжину масиву fruits,
+//  використовуючи властивість length.
+
+// 4. Оголоси дві змінні: lastElementIndex - очікуване значення: індекс
+// останнього елемента масиву fruits через довжина_масиву - 1; lastElement -
+// значення останнього елемента масиву;
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];
+
+fruits[1] = "peach";
+fruits[3] = "banana";
+
+const fruitsArrayLength = fruits.length;
+
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[fruits.length - 1];
+
+console.log(fruits);
+console.log(fruitsArrayLength);
+console.log(lastElementIndex);
---
+++ js/check-storage.js
@@ -0,0 +1,33 @@
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } else if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+
+//   return "The order is accepted, our manager will contact you";
+//   // Change code above this line
+// }
+
+function checkStorage(available, ordered) {
+  // Change code below this line
+
+  if (ordered === 0) {
+    return "Your order is empty!";
+  }
+
+  if (ordered > available) {
+    return "Your order is too large, not enough goods in stock!";
+  }
+
+  return "The order is accepted, our manager will contact you";
+  // Change code above this line
+}
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 250));
+console.log(checkStorage(150, 0));
